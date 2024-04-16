b0e36dd14164d9c8d0db6b034a0afbfa808f5183
Kyle Bashour <kylebshr@me.com>
2024-04-16 15:52:00 -0700

    Add project

---
+++ Glimpse/GlimpseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlimpseApp.swift
+//  Glimpse
+//
+//  Created by Kyle Bashour on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct GlimpseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
