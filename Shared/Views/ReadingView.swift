74a53079482b76b96dfd86c9919912e7d734e508
Kyle Bashour <kylebshr@me.com>
2025-10-23 11:12:19 -0700

    Improve reading view

--- Shared/Views/ReadingView.swift
+++ Shared/Views/ReadingView.swift
@@ -20,34 +20,47 @@ struct ReadingView: View {
         (reading?.value ?? 100).formatted(.glucose(unit))
     }
 
-    var body: some View {
-        HStack(spacing: 0) {
-            Text(text)
-                .contentTransition(.numericText(value: Double(reading?.value ?? 0)))
+    var image: Image? {
+        if !redactionReasons.contains(.placeholder), let image = reading?.image {
+            return image
+        } else {
+            return nil
+        }
+    }
 
-            if !redactionReasons.contains(.placeholder), let image = reading?.image {
-                Text(" ")
-                image.imageScale(.small)
-                    .contentTransition(.symbolEffect(.replace))
-                    .transition(.blurReplace)
+    var body: some View {
+        ZStack {
+            if let image {
+                Text("\(text) \(image)")
+            } else {
+                Text(text)
             }
         }
+        .contentTransition(.numericText(value: Double(reading?.value ?? 0)))
         .redacted(reason: reading == nil ? .placeholder : [])
+        .imageScale(.small)
+        .animation(.default, value: reading)
     }
 }
 
 #Preview {
-    VStack {
-        ReadingView(reading: .placeholder)
-            .fontWeight(.semibold)
+    @Previewable @State var value = GlucoseReading(value: 89, trend: .fortyFiveUp, date: .now)
 
-        ReadingView(reading: nil)
-            .fontWeight(.semibold)
+        VStack {
+            ReadingView(reading: value)
+                .fontWeight(.semibold)
 
-        ReadingView(reading: .placeholder)
-            .font(.largeTitle)
+            ReadingView(reading: nil)
+                .fontWeight(.semibold)
 
-        ReadingView(reading: nil)
-            .font(.largeTitle)
-    }
+            ReadingView(reading: value)
+                .font(.largeTitle)
+
+            ReadingView(reading: nil)
+                .font(.largeTitle)
+
+            Button("Update") {
+                value = GlucoseReading(value: 92, trend: .flat, date: .now)
+            }
+        }
 }
